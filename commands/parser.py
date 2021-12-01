952b89e8b92377f47dc236c7696e9dcd1d1291c3
Jonas Chow <jonaschow99@gmail.com>
2021-12-01 17:20:23 +0800

    add reaction

--- commands/parser.py
+++ commands/parser.py
@@ -41,4 +41,6 @@ class Parser():
         cmd.set_arguments(content.removeprefix(commandWord), message.author.id)
         if isinstance(cmd, HelpCommand):
             cmd.set_commands(Parser.commands)
+        message.add_reaction('✅')
+
         return cmd
